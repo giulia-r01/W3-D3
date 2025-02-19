c9041bfe99b18c5bb9296d88bc51b27578b6abe8
Giulia Rizzo <girzzo@gmail.com>
2025-02-19 16:47:45 +0100

    Task completed

---
+++ js/tasklist.js
@@ -0,0 +1,33 @@
+const formTask = document.getElementById("add-task")
+formTask.addEventListener("submit", (e) => {
+  e.preventDefault()
+
+  const addNewTask = document.getElementById("new-task")
+
+  const search = { TheTask: addNewTask.value }
+  console.log("valore", search)
+
+  const cardWithTask = document.createElement("ul")
+  cardWithTask.innerHTML = `
+  <li onclick="lineThrough(event)">${search.TheTask}</li>
+  <button onclick="deleteLi(event)">Elimina task</button>
+  `
+
+  const listTask = document.getElementById("lista")
+  listTask.appendChild(cardWithTask)
+
+  addNewTask.value = ""
+})
+
+const lineThrough = function (e) {
+  console.log("Barra", e.target.parentElement)
+  const pressedLi = e.target
+  const strikethrough = pressedLi.parentElement
+  strikethrough.classList.add("testo-barrato")
+}
+const deleteLi = function (e) {
+  console.log("Barra", e.target.parentElement)
+  const pressedButton = e.target
+  const deleteButton = pressedButton.parentElement
+  deleteButton.remove()
+}
